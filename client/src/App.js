df202386fd6ef9784fe8bc007eaf12395c7b4552
german2717@gmail.com <german2717@gmail.com>
2023-07-14 11:33:51 -0300

    trabaje en App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
-import Home from "./Views/Home";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./Views/FormLogin.jsx";
+import Home from "./Views/Home";
+import Detail from "./Views/Detail";
+import SearchResult from "./Views/SearchResult";
+import Admin from "./Views/Admin";
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/search" element={<SearchResult />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
     </div>
   );
 }
