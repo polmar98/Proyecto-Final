b1ad6bdb57947809e1b78dddf4d1c51a1890bcdb
German <german2717@gmail.com>
2023-07-15 14:21:26 -0300

    arreglo

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,20 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./Views/FormLogin.jsx";
-<<<<<<< HEAD
 import Home from "./Views/Home";
 import Detail from "./Views/Detail";
 import SearchResult from "./Views/SearchResult";
 import Admin from "./Views/Admin";
-=======
-import Detail from "./Views/Detail";
-import {Route, Routes} from "react-router-dom";
->>>>>>> moichubr
 
 function App() {
   return (
     <div className="App">
       <Routes>
-<<<<<<< HEAD
         <Route exact path="/" element={<Home />} />
         <Route path="/detail" element={<Detail />} />
         <Route path="/search" element={<SearchResult />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/admin" element={<Admin />} />
-=======
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/?" element= {<LoginPage />}/>
-        <Route path="/detail" element={<Detail/>}/>
-
-
->>>>>>> moichubr
       </Routes>
     </div>
   );
