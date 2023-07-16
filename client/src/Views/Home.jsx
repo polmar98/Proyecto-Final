7a96521c1cc442578c54597c51d6d6bf8f586f7a
German <german2717@gmail.com>
2023-07-16 11:16:56 -0300

    footer creado, home con footer

--- client/src/Views/Home.jsx
+++ client/src/Views/Home.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import NavBar from "../Components/NavBar";
 import SearchBar from "../Components/SearchBar";
 import ContFilter from "../Components/ContFilter";
+import Footer from "../Components/Footer";
 
 function Home() {
   return (
-    <div className=" bg-local h-screen bg-img portada">
-      <div>
-        <NavBar />
+    <>
+      <div className=" bg-local h-screen bg-img portada">
+        <div>
+          <NavBar />
+        </div>
+        <div className="items-center flex justify-center mt-56">
+          <SearchBar />
+        </div>
       </div>
-      <div className="items-center flex justify-center mt-56">
-        <SearchBar />
-      </div>
-      <div className=" mt-[450px] items-center flex justify-center">
+
+      <div className=" mt-10 items-center flex justify-center">
         <ContFilter />
       </div>
-    </div>
+      <div className="mt-[80px]">
+        <Footer />
+      </div>
+    </>
   );
 }
 
