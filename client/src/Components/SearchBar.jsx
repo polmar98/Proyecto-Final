831502f4e528caf2630ea9d5c71bdba6b202d2bf
SARA CAROLINA MARULANDA ROJAS <sara.marulanda@prebel.com.co>
2023-07-17 14:35:05 -0500

    searchbar

--- client/src/Components/SearchBar.jsx
+++ client/src/Components/SearchBar.jsx
@@ -1,39 +1,43 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { searchPackages } from "../Redux/packagesSlice";
+import { setSearchResults, searchPackagesAsync } from "../Redux/packagesSlice";
 import { useNavigate } from "react-router-dom";
 
 
 function SearchBar() {
 
-  const dispatch= useDispatch()
-  const navigate = useNavigate ()
-  const[pais,setPais]=useState("")
+  const [word, setWord] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleInputChange = (event) => {
+    setWord(event.target.value);
+  };
+
+  const handleSearch = () => {
+    dispatch(searchPackagesAsync(word))
+        navigate("/search"); // Opcional: redirigir a la página de resultados de búsqueda
+        setWord("")
+      };
   
 
-  const handleSearch =()=>{
-    
-    dispatch(searchPackages(pais))
-    navigate ("/search")
-  }
-
-  const handleInputChange=(e)=>{
-    setPais(e.target.value)
-  }
-
-
+  
   return (
     <div className="flex bg-white w-[400px] h-[50px] rounded justify-between items-center">
       <input
         type="text"
-        placeholder="Country or Destination"
+        placeholder="A donde ..."
         className=" bg-white rounded p-2 m-2 w-[250px] focus:outline-none"
-        value={pais}
+        value={word}
         onChange={handleInputChange}
       />
       <button className=" bg-green-300 rounded p-2 m-2"
       onClick={handleSearch}
-      >Find Trip Now</button>
+      >Buscar</button>
+
+<button className=" bg-green-300 rounded p-2 m-2"
+      onClick={handleSearch}
+      >Todos</button>
     </div>
   );
 }
