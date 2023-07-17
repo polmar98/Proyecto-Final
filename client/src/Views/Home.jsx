1d60978723c06332db3e48fb56e8bd1eda7320a4
German <german2717@gmail.com>
2023-07-17 14:50:28 -0300

    cambios en home

--- client/src/Views/Home.jsx
+++ client/src/Views/Home.jsx
@@ -3,8 +3,19 @@ import NavBar from "../Components/NavBar";
 import SearchBar from "../Components/SearchBar";
 import ContFilter from "../Components/ContFilter";
 import Footer from "../Components/Footer";
+import { fetchPackages, selectPackages } from "../Redux/packagesSlice";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 function Home() {
+  const dispatch = useDispatch();
+  const packages = useSelector(selectPackages);
+  console.log(packages);
+
+  useEffect(() => {
+    dispatch(fetchPackages());
+  }, [dispatch]);
+
   return (
     <>
       <div className=" bg-local h-screen bg-img portada">
