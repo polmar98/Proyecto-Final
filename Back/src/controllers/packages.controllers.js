f0bdecfe2e45f5a9a2f16b3b1896f6c876e3dbd1
pol <polmn98@gmail.com>
2023-07-17 08:09:28 -0500

    Se agrega ruta para busquedas por titulo

--- Back/src/controllers/packages.controllers.js
+++ Back/src/controllers/packages.controllers.js
@@ -38,9 +38,7 @@ const addPackages = async(objeto) => {
         }
         await Activity.create(newActivity);
     })
-
     return packageCreated;
-
 };
 
 
@@ -69,12 +67,11 @@ const mapList = (array) => array.map((result) => {
         include: [
             { association:'TypePackage', attributes:['id', 'name']},
             { association:'Airline', attributes:['id', 'name']},
-            { association:'City', attributes:['id', 'name']},
+            { association:'City', attributes:['id', 'name', 'idCountry']},
             { association:'Hotel', attributes:['id', 'name', 'image']},
             { association:'Continent', attributes:['id', 'name']},
             { model: Activity},
         ]},
-   
     );    
     return paquetes;
 };
@@ -86,7 +83,7 @@ const getPackageById = async(idp) => {
     include: [
         { association:'TypePackage', attributes:['id', 'name']},
         { association:'Airline', attributes:['id', 'name']},
-        { association:'City', attributes:['id', 'name']},
+        { association:'City', attributes:['id', 'name', 'idCountry']},
         { association:'Hotel', attributes:['id', 'name', 'stars', 'image', 'calification', 'details']},
         { association:'Continent', attributes:['id', 'name']},
         { model: Activity},     
@@ -95,4 +92,31 @@ const getPackageById = async(idp) => {
    return paquete;
 };
 
-module.exports = { addPackages, viewPackages, getPackageById };
+
+//esta funcion hace una busqueda de paquetes en el campo titulo
+const searchPackages = async(search) => {
+    const paquetes = await Package.findAll({
+        where: {active: true},
+        include: [
+            { association:'TypePackage', attributes:['id', 'name']},
+            { association:'Airline', attributes:['id', 'name']},
+            { association:'City', attributes:['id', 'name', 'idCountry']},
+            { association:'Hotel', attributes:['id', 'name', 'image']},
+            { association:'Continent', attributes:['id', 'name']},
+            { model: Activity},
+        ]},
+   
+    );  
+    const packages = paquetes.filter(e => e.title.toLowerCase().includes(search.toLowerCase()))
+    return packages;
+};
+
+//la sgte rutina almacena de forma masiva paquetes enviados desde un array
+const addMassivePackages = async(array) => {
+    array.forEach(async(objeto) => {
+        await addPackages(objeto);
+    });
+    return "Array de paquetes cargado";
+};
+
+module.exports = { addPackages, viewPackages, getPackageById, searchPackages, addMassivePackages };
--- Back/src/handlers/packages.handlers.js
+++ Back/src/handlers/packages.handlers.js
@@ -1,17 +1,29 @@
 const { Router } = require('express');
-const {addPackages, viewPackages, getPackageById} = require('../controllers/packages.controllers');
+const {addPackages,
+      viewPackages,
+      getPackageById,
+      searchPackages,
+      addMassivePackages} = require('../controllers/packages.controllers');
 const router = Router();
 
+//handler para busqueda de paquetes. si se agrega un query de busqueda ejecuta searchPackages
 router.get('/', async(req, res) => {
+   const search = req.query.title;
    try {
-       const result = await viewPackages();
-       res.status(200).json(result);
+       if(!search) {
+           const result = await viewPackages();
+           res.status(200).json(result);
+       } else {
+           const result = await searchPackages(search);
+           res.status(200).json(result);
+       };
+      
    } catch (error) {
        res.status(500).json({message: error.message});
    } 
-
 });
 
+//handler para busqueda de paquetes mediante el id
 router.get('/:id', async(req, res) => {
    const { id } = req.params; 
    try {
@@ -22,6 +34,7 @@ router.get('/:id', async(req, res) => {
    }
 });
 
+//handler para agregar paquetes
 router.post('/', async(req, res) => {
     const objeto = req.body;
     try {
@@ -30,6 +43,19 @@ router.post('/', async(req, res) => {
     } catch (error) {
         res.status(500).json({message: error.message});        
     }
-})
+});
+
+//handler para agregar paquetes de forma masiva
+router.post('/massive', async(req, res) => {
+    const array = req.body;
+    try {
+            const result = await addMassivePackages(array);
+            res.status(200).json(result);            
+        } catch (error) {
+            res.status(500).json({message: error.message});                    
+        }
+});
+
+
 
 module.exports = router;
