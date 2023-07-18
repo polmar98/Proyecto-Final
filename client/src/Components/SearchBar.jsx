8a7838f96b49a341b3c7984bd7e0d3724edeec76
LucasAonzo <lucasaonzo@gmail.com>
2023-07-18 16:16:15 -0300

    Actualizando estilos de componentes y creando about

--- client/src/Components/SearchBar.jsx
+++ client/src/Components/SearchBar.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { setSearchResults, searchPackagesAsync } from "../Redux/packagesSlice";
 import { useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
+import { IoLocationSharp } from "react-icons/io5";
 
 function SearchBar() {
   const [word, setWord] = useState("");
@@ -23,7 +24,10 @@ function SearchBar() {
 
   return (
     <div className="flex bg-white w-[400px] h-[50px] rounded justify-between items-center shadow-md">
-      <div className="flex-grow">
+      <div className="flex items-center">
+        <IoLocationSharp
+          style={{ fontSize: "20px", marginLeft: "10px", marginRight: "5px" }}
+        />
         <input
           type="text"
           placeholder="A donde ..."
@@ -33,7 +37,7 @@ function SearchBar() {
         />
       </div>
       <div>
-        <button className="bg-green-300 rounded p-2 m-2" onClick={handleSearch}>
+        <button className="bg-green-400 rounded p-2 m-2" onClick={handleSearch}>
           <FaSearch style={{ fontSize: "20px", color: "white" }} />
         </button>
       </div>
