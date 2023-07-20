a9a7b45b860823cb112c033d6625fe33bee46a02
moichubr <brunmoira@gmail.com>
2023-07-20 19:49:11 -0300

    detail listo

--- client/src/Components/Activities.jsx
+++ client/src/Components/Activities.jsx
@@ -1,35 +1,50 @@
 import React from "react";
-import data from "../packages";
 
-function Activities() {
-  const idrequired = 2;
-  const tour = data.find((p) => p.Id === idrequired);
+function Activities({ activity }) {
+  const { Activities } = activity;
 
   return (
-    <div className="m-10">
-      <h2 className="text-4xl font-bold">Actividades</h2>
-      {tour.Servicios.map((el) => (
-        <div key={el.Detalle} className="inline-flex flex-col m-1">
-          {el.Url ? (
-            <img
-              src={el.Url}
-              alt="Img not found"
-              className="w-[400px] h-[300px] rounded-lg "
-            />
-          ) : null}
-          <h3 className="text-xl font-sans font-semibold">{el.Detalle}</h3>
-          {!el.Incluido ? (
-            <h4 className="text-xl font-bold">
-              Precio:
-              {tour.Moneda}
-              {el.Precio}
-            </h4>
-          ) : (
-            <h4 className="text-xl font-bold">Actividad incluida</h4>
-          )}
-        </div>
-      ))}
-    </div>
+    <>
+      <h2 className="text-xl font-bold mb-4 mt-12 text-left fontPoppins">
+        Actividades
+      </h2>
+      <div className="grid grid-cols-2 gap-4 mt-8">
+        {Activities?.map((el) => (
+          <div
+            key={el.id}
+            className="block rounded-lg bg-white shadow-xl border border-gray-300 p-4 card hover:scale-110 hover:z-20"
+          >
+            <a href="#!">
+              <img
+                className="rounded-t-lg w-full h-auto"
+                src={
+                  el.image
+                    ? el.image
+                    : "https://uss.com.ar/sitio/wp-content/themes/consultix/images/no-image-found-360x260.png"
+                }
+                alt="Img not found"
+              />
+            </a>
+            <div className="text-center fontPoppins">
+              <h5 className="mb-4 mt-2 text-m font-bold leading-tight text-gray-800">
+                {el.name}
+              </h5>
+
+              <div className="flex justify-between">
+                <p className="mb-2 font-small ml-2 text-gray-600">
+                  Duración: {el.duration}
+                </p>
+                {!el.included ? (
+                  <span className="font-small mb-2 mr-2">USD {el.price}</span>
+                ) : (
+                  <span>Actividad incluida</span>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
