5c90347517c4fc10011417cc7965ea5316402d55
moichubr <brunmoira@gmail.com>
2023-07-23 18:53:32 -0300

    carrito style y delete item

--- client/src/Components/CartItem.jsx
+++ client/src/Components/CartItem.jsx
@@ -1,33 +1,85 @@
-const CartItem = ({props}) => {
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { FiTrash2 } from "react-icons/fi"; // Import the trash icon from react-icons
 
-    return(
-    
-        <div className="rounded-lg bg-white shadow-xl border border-teal-900 p-4 grid grid-cols-2 w-3/4 ml-10">
-            <div>
-            <img src={props.image} alt='Img not found' className="w-2/4 h-auto"/>
-            </div>
-            <div>
-            <h1>{props.title}</h1>
-            <h3>USD {props.price} por persona</h3>
+const CartItem = ({ props }) => {
+  const user = useSelector((state) => state.users.user);
+  const navigate = useNavigate();
 
-            <div className="w-auto h-auto">
-            <label>Cantidad:</label>
-            <input id="number" type="number" value={props.amount} className="w-1/5"/>
-            </div>
+  function clearItem() {
+    const userConfirm = window.confirm(
+      "Se elimirá este item del carrito, quieres continuar?"
+    );
+    if (userConfirm && !user) {
+      let localStorageJSON = localStorage.getItem("carrito");
+      // console.log('JSON', localStorageJSON)
+      let storedItems = [];
+      if (localStorageJSON !== null) {
+        storedItems = JSON.parse(localStorageJSON); //convierte a JS
+        // console.log('js', storedItems)
+      }
+      const filteredCart = storedItems.filter((item) => item.id !== props.id);
+      const updatedItemsJSON = JSON.stringify(filteredCart);
+      localStorage.setItem("carrito", updatedItemsJSON); //lo convierte a json
+    }
+    navigate("/shoppingCart");
+  }
 
-            <div>
-            <label>Agrega actividades: </label>
-            <select>
-            {props.activities.map(el =>
-                <option key={el.id}>{el.name} por USD{el.price}</option>)}
+  return (
 
-            </select>
-            </div>
-            <button>Eliminar paquete</button>
-            </div>
+    <div className="rounded-lg bg-white shadow-xl border border-green-500 p-4 grid grid-cols-2 w-full justify-center mb-4">
+      <div>
+        <img
+          src={props.image}
+          alt="Img not found"
+          className="w-3/4 h-auto justify-center align-middle rounded"
+        />
+      </div>
+      <div className="flex flex-col justify-between text-left">
+        <div>
+          <h1 className="text-xl font-semibold">{props.title}</h1>
+          <h3 className="text-green-700">USD {props.price} por persona</h3>
         </div>
-    
-    )
+
+        <div className="w-auto h-auto mt-2">
+          <label className="text-green-700 font-semibold">Cantidad:</label>
+          <input
+            id="number"
+            type="number"
+            value={props.amount}
+            className="w-1/5 border border-gray-300 rounded-md px-2 py-1 ml-2"
+          />
+        </div>
+
+        <div className="mt-2">
+        <h3 className="text-green-700 font-semibold">Agrega actividades</h3>
+          {/* <select className="border border-gray-300 rounded-md px-2 py-1 ml-2"> */}
+          <ul>
+            {props.activities.map((el) =>
+              !el.included ? 
+                <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                <div className="flex items-center pl-3">
+                    <input id={el.id} type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                    <label for={el.id} className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{el.name} por USD{el.price}</label>
+                </div> 
+                </li> : null)
+            }
+            </ul>
+
+        </div>
+        <button
+          onClick={() => {
+            clearItem();
+          }}
+          className=" text-right w-14 mt-4 bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-2 rounded-md flex items-center justify-center"
+        >
+          <FiTrash2 className="mr-2" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
-export default CartItem
+export default CartItem;
+
+
--- client/src/Views/ShoppingCart.jsx
+++ client/src/Views/ShoppingCart.jsx
@@ -3,6 +3,8 @@ import CartItem from "../Components/CartItem";
 import { useSelector, useDispatch } from "react-redux";
 // import { useState, useEffect } from "react";
 import { clean_cart } from "../Redux/ShoppingCart/shoppingCartActions";
+import { Link } from "react-router-dom";
+import { AiOutlineCheckCircle, AiOutlineShopping, AiOutlineDelete } from "react-icons/ai";
 
 
 const ShoppingCart = () => {
@@ -11,11 +13,10 @@ const ShoppingCart = () => {
   // console.log('el estado carrito', {carrito})
   const navigate = useNavigate();
   const dispatch = useDispatch();
-//   const [cartState, setcartState] = useState()
-
-let localStorageItems = JSON.parse(localStorage.getItem("carrito")); //convierte a JS
 
 
+//convierte a JS el localStorage
+let localStorageItems = JSON.parse(localStorage.getItem("carrito"));
 
 
   //vaciar el carrito
@@ -25,8 +26,8 @@ let localStorageItems = JSON.parse(localStorage.getItem("carrito")); //convierte
       ); //boolean
       if (userConfirm && !user) {
         localStorage.clear("carrito");
-        alert("El carrito fue vaciado con éxito.");
         navigate('/shoppingCart');
+        alert("El carrito fue vaciado con éxito.");
       } 
       if (userConfirm && user) {
          dispatch(clean_cart()).catch((error) => {
@@ -36,28 +37,83 @@ let localStorageItems = JSON.parse(localStorage.getItem("carrito")); //convierte
       else return;
   }
 
-
-
   return (
-    <div>
-      <h1>El carrito de compras</h1>
-        {user ? cartItems?.map((el, index) => (
-        <CartItem key={index} props={el} />
-           )) : localStorageItems?.map((el, index) => (
+    <div className="grid grid-cols-4">
+    <div className="w-full flex flex-col col-span-3 mt-4">
+      {user
+        ? cartItems?.map((el, index) => <CartItem key={index} props={el} />)
+        : localStorageItems?.map((el, index) => (
             <CartItem key={index} props={el} />
-               ))}
+          ))}
+
+      <div className="grid grid-col-2 gap-4  justify-center ">
+
+        <div>
+        <button className="bg-green-700 hover:bg-green-800 text-white py-2 px-4 m-2 rounded w-full flex items-center justify-center">
+          <AiOutlineCheckCircle className="mr-2" />
+          COMPLETAR EL PAGO
+        </button>
+        </div>
         
-      <button>COMPLETAR EL PAGO</button>
-      <button
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        SEGUIR COMPRANDO
-      </button>
-      <button onClick={() => {clearCart()}}>VACIAR CARRITO</button>
+        <div className="flex flex-row justify-center items-center">
+        <Link
+          to="/" // Update this with the path for continuing shopping
+          className="bg-gray-200 hover:bg-gray-300 text-gray-600 text-xs py-2 px-4 m-2 rounded flex items-center justify-center w-1/3"
+        >
+          <AiOutlineShopping className="mr-2 text-2xl" />
+          SEGUIR COMPRANDO
+        </Link>
+        <button
+          onClick={() => {
+            clearCart();
+          }}
+          className="bg-red-500 hover:bg-red-600 text-white text-xs py-2 px-4 m-2 rounded flex items-center justify-center"
+        >
+          <AiOutlineDelete className="mr-2 text-2xl" />
+          VACIAR CARRITO
+        </button>
+        </div>
+      </div>
+    </div>
+
+          <div className="col-span-1 mt-4">
+            <div>
+            <h1>Resumen de compra</h1>
+            <span></span>
+            </div>
+            </div>
+
     </div>
   );
 };
 
 export default ShoppingCart;
+
+
+
+//   return (
+//     <div className="w-6/8 flex flex-col">
+      
+//         {user ? cartItems?.map((el, index) => (
+//         <CartItem key={index} props={el} />
+//            )) : localStorageItems?.map((el, index) => (
+//             <CartItem key={index} props={el} />
+//                ))}
+        
+//         <div className="inline-flex w-2/5 fontPoppins">
+//       <button  className="bg-green-700 hover:bg-green-800 text-white py-1 px-1 m-2 rounded w-full">COMPLETAR EL PAGO</button>
+//       <button
+//         onClick={() => {
+//           navigate("/");
+//         }}
+//         className="bg-gray-200 w-full hover:bg-gray-300 text-gray-600 font-xs py-1 px-1 m-2 rounded">
+//         SEGUIR COMPRANDO
+//       </button>
+//       <button onClick={() => {clearCart()}} className="bg-red-500 w-full hover:bg-red-600 text-white font-xs py-1 px-1 m-2 rounded">VACIAR CARRITO</button>
+//       </div>
+//     </div>
+
+//   );
+// };
+
+// export default ShoppingCart;
