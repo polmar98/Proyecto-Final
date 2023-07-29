dc374cd2dd78b72941fe6e7cb0e93e31f0a39386
LucasAonzo <lucasaonzo@gmail.com>
2023-07-29 15:21:55 -0300

    actualizando profile

--- client/src/Components/Userprofile/UserDetail.jsx
+++ client/src/Components/Userprofile/UserDetail.jsx
@@ -61,10 +61,10 @@ const UserDate = () => {
           </label>
           <div className="mt-2">
             <input
-              value={user.firstName}
+              value={user.name}
               type="text"
-              name="first-name"
-              id="first-name"
+              name="last-name"
+              id="last-name"
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
           </div>
@@ -79,6 +79,7 @@ const UserDate = () => {
           </label>
           <div className="mt-2">
             <input
+              value={user.lastName}
               type="text"
               name="last-name"
               id="last-name"
@@ -97,6 +98,7 @@ const UserDate = () => {
           </label>
           <div className="mt-2">
             <input
+              value={user.email}
               id="email"
               name="email"
               type="email"
