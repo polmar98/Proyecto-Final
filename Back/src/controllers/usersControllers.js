9f9aacc5e4f3789c9aff1276173254f50f1ece0c
cristian molina <cristian84molina@gmail.com>
2023-07-29 19:32:16 -0300

    User: se agrego metodo PUT

--- Back/src/controllers/usersControllers.js
+++ Back/src/controllers/usersControllers.js
@@ -45,9 +45,35 @@ const getUserByUid = async (uid) => {
   return user;
 };
 
+
+// Controller para modificar un usuario existente
+const updateUser = async (id, newData) => {
+  try {
+    const user = await User.findByPk(id);
+
+    if (!user) {
+      throw new Error("Usuario no encontrado");
+    }
+
+    // Actualizar los atributos del usuario con los datos proporcionados en newData
+    // Por ejemplo, si newData es un objeto con los atributos que se desean modificar:
+    // { name: "Nuevo nombre", lastName: "Nuevo apellido", email: "nuevo@ejemplo.com" }
+    // Esto actualizará únicamente los atributos proporcionados y dejará los demás sin cambios.
+    await user.update(newData);
+
+    return { message: "Usuario modificado exitosamente" };
+  } catch (error) {
+    // Si ocurre algún error durante la modificación, puedes manejarlo aquí.
+    console.error("Error al modificar el usuario:", error);
+    throw new Error("Hubo un problema al modificar el usuario");
+  }
+};
+
+
 module.exports = {
   createUser,
   getAllUsers,
   deleteUser,
   getUserByUid,
+  updateUser
 };
