77c42ca3c7cfc2afc41b1da9d94f62b6f9a95508
LucasAonzo <lucasaonzo@gmail.com>
2023-08-01 22:10:42 -0300

    solucionando color card de la review en detail

--- client/src/Components/Review.jsx
+++ client/src/Components/Review.jsx
@@ -1,61 +1,95 @@
 import React from "react";
 // import { useSelector } from "react-redux";
 
-function Review({coments}) {
+function Review({ coments }) {
   // const {reviewData} = coments
-//   console.log('review en componente', reviewData)
+  //   console.log('review en componente', reviewData)
 
   return (
-	<>
-	<div className="mb-2 mt-8 md:p-2 md:px-2 ">
-		<h1 className="text-xl font-bold text-left fontPoppins">Opiniones de otros viajeros:</h1>
-	</div>
-    <section className="my-2 dark:bg-verdeFooter dark:text-gray-100 fontPoppins">
+    <>
+      <div className="mb-2 mt-8 md:p-2 md:px-2 ">
+        <h1 className="text-xl font-bold text-left fontPoppins">
+          Opiniones de otros viajeros:
+        </h1>
+      </div>
+      <section className="my-2  dark:text-gray-100 fontPoppins">
+        <div className="container grid grid-cols-2 gap-4 items-center justify-center mx-auto lg:flex-row lg:flex-wrap lg:justify-evenly lg:px-10">
+          <div className="flex flex-col max-w-sm mx-4 my-2 shadow-lg rounded-xl ">
+            <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-verdeFooter">
+              <p className="relative px-6 py-1 text-lg italic text-center text-gray-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="w-8 h-8 text-green-600"
+                >
+                  <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                  <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                </svg>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Voluptatibus quibusdam, eligendi exercitationem molestias
+                possimus facere.
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="absolute right-0 w-8 h-8 text-green-600"
+                >
+                  <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                  <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                </svg>
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center p-8 rounded-b-lg  text-gray-900">
+              <img
+                src="https://source.unsplash.com/50x50/?portrait?1"
+                alt=""
+                className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full bg-gray-500 "
+              />
+              <p className="text-xl font-semibold leadi">Fernando Bouvet</p>
+              <p className="text-sm uppercase">05/03/2022</p>
+            </div>
+          </div>
+          <div className="flex flex-col max-w-sm mx-4 my-2 shadow-lg rounded-xl">
+            <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-verdeFooter">
+              <p className="relative px-6 py-1 text-lg italic text-center text-gray-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="w-8 h-8 text-green-600"
+                >
+                  <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                  <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                </svg>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Voluptatibus quibusdam, eligendi exercitationem molestias
+                possimus facere.
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="absolute right-0 w-8 h-8 text-green-600"
+                >
+                  <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                  <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                </svg>
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center p-8 rounded-b-lg  dark:text-gray-900">
+              <img
+                src="https://source.unsplash.com/50x50/?portrait?2"
+                alt=""
+                className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full bg-gray-500 "
+              />
+              <p className="text-xl font-semibold leadi">Carla Petersen</p>
+              <p className="text-sm uppercase">23/09/2022</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
 
-	<div className="container grid grid-cols-2 gap-4 items-center justify-center mx-auto lg:flex-row lg:flex-wrap lg:justify-evenly lg:px-10">
-		<div className="flex flex-col max-w-sm mx-4 my-2 shadow-lg">
-			<div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-verdeFooter">
-				<p className="relative px-6 py-1 text-lg italic text-center text-gray-100">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 text-green-600">
-						<path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
-						<path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-					</svg>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus quibusdam, eligendi exercitationem molestias possimus facere.
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 text-green-600">
-						<path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
-						<path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
-					</svg>
-				</p>
-			</div>
-			<div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-verdeFooter dark:text-gray-900">
-				<img src="https://source.unsplash.com/50x50/?portrait?1" alt="" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full bg-gray-500 " />
-				<p className="text-xl font-semibold leadi">Fernando Bouvet</p>
-				<p className="text-sm uppercase">05/03/2022</p>
-			</div>
-		</div>
-		<div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
-			<div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-verdeFooter">
-				<p className="relative px-6 py-1 text-lg italic text-center text-gray-100">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 text-green-600">
-						<path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
-						<path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-					</svg>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus quibusdam, eligendi exercitationem molestias possimus facere.
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 text-green-600">
-						<path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
-						<path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
-					</svg>
-				</p>
-			</div>
-			<div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-verdeFooter dark:text-gray-900">
-				<img src="https://source.unsplash.com/50x50/?portrait?2" alt="" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full bg-gray-500 " />
-				<p className="text-xl font-semibold leadi">Carla Petersen</p>
-				<p className="text-sm uppercase">23/09/2022</p>
-			</div>
-		</div> 
-	</div>
-</section>
-</>
-    );
-  }
-  
-  export default Review;
-  
+export default Review;
