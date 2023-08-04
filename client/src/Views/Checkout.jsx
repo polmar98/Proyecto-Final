59f9314f0ace25cb65ab45bc4bbd725aee425d80
moichubr <brunmoira@gmail.com>
2023-08-04 16:01:40 -0300

    idioma checkout

--- client/src/Views/Checkout.jsx
+++ client/src/Views/Checkout.jsx
@@ -12,7 +12,7 @@ import { create_order } from "../Redux/Checkout/checkoutActions";
 
 export default function Checkout() {
   const { currentUser } = useContext(authContext);
-  console.log('el user en checkout', currentUser.uid)
+  // console.log('el user en checkout', currentUser.uid)
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -23,7 +23,7 @@ export default function Checkout() {
 
   // const idCart = useSelector((state) => state.carrito.idCart);
   const cartItems = useSelector((state) => state.carrito.cart);
-  console.log("STO ES cartItems DESDE SHOPING CART ", cartItems);
+  // console.log("STO ES cartItems DESDE SHOPING CART ", cartItems);
   // console.log("STO ES idCart DESDE SHOPING CART ", idCart);
   const vatPercentage = 10; // Porcentaje del impuesto
   let totalPrice = cartItems.reduce(
@@ -35,13 +35,13 @@ export default function Checkout() {
   const precioFormateado = finalPrice.toFixed(2); // Precio total con impuestos
   const vatFormateado = vatAmount.toFixed(2); // Impuesto formateado
   const precioJSON = precioFormateado.toString(); //precio para colocar en la order
-  console.log("precio final json", precioJSON);
+  // console.log("precio final json", precioJSON);
 
   const itemTitles = cartItems
     .map((el) => el.title)
     .concat()
     .toString();
-  console.log("items a enviar", itemTitles);
+  // console.log("items a enviar", itemTitles);
 
   const order = {
     intent: "CAPTURE",
@@ -78,7 +78,7 @@ export default function Checkout() {
           <div class="col-span-full py-6 px-4 sm:py-12 lg:col-span-6 lg:py-24">
             <div class="mx-auto w-full max-w-lg">
               <h1 class="relative text-2xl font-medium text-gray-700 sm:text-3xl">
-                Secure Checkout
+                Checkout Seguro
                 <span class="mt-2 block h-1 w-10 bg-teal-600 sm:w-20"></span>
               </h1>
               <div
@@ -90,9 +90,9 @@ export default function Checkout() {
                     <CgDanger className="mr-2 text-2xl" />
                   </div>
                   <div>
-                    <p className="font-bold">Attention</p>
+                    <p className="font-bold">Atención</p>
                     <p className="text-sm">
-                      Currently, the only functioning payment method is PayPal.
+                      Momentáneamente, el único método de pago en funcionamiento es PayPal.
                     </p>
                   </div>
                 </div>
@@ -118,7 +118,7 @@ export default function Checkout() {
                     for="card-number"
                     class="text-xs font-semibold text-gray-500"
                   >
-                    Card number
+                    Número de tarjeta
                   </label>
                   <input
                     type="text"
@@ -135,36 +135,36 @@ export default function Checkout() {
                 </div>
                 <div>
                   <p class="text-xs font-semibold text-gray-500">
-                    Expiration date
+                    Fecha de vencimiento
                   </p>
                   <div class="mr-6 flex flex-wrap">
                     <div class="my-1">
                       <label for="month" class="sr-only">
-                        Select expiration month
+                        Selecciona el mes de vencimiento
                       </label>
                       <select
                         name="month"
                         id="month"
                         class="cursor-pointer rounded border-gray-300 bg-gray-50 py-3 px-2 text-sm shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
                       >
-                        <option value="">Month</option>
+                        <option value="">Mes</option>
                       </select>
                     </div>
                     <div class="my-1 ml-3 mr-6">
                       <label for="year" class="sr-only">
-                        Select expiration year
+                        Selecciona el año de vencimiento
                       </label>
                       <select
                         name="year"
                         id="year"
                         class="cursor-pointer rounded border-gray-300 bg-gray-50 py-3 px-2 text-sm shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
                       >
-                        <option value="">Year</option>
+                        <option value="">Año</option>
                       </select>
                     </div>
                     <div class="relative my-1">
                       <label for="security-code" class="sr-only">
-                        Security code
+                        Código de seguridad
                       </label>
                       <input
                         type="text"
@@ -178,7 +178,7 @@ export default function Checkout() {
                 </div>
                 <div>
                   <label for="card-name" class="sr-only">
-                    Card name
+                    Nombre de tarjeta
                   </label>
                   <input
                     type="text"
@@ -190,12 +190,12 @@ export default function Checkout() {
                 </div>
               </form>
               <p class="mt-10 text-center text-sm font-semibold text-gray-500">
-                By placing this order you agree to the{" "}
+                Al comprar esta orden, aceptas los{" "}
                 <a
                   href="#"
                   class="whitespace-nowrap text-teal-400 underline hover:text-teal-600"
                 >
-                  Terms and Conditions
+                  Términos y Condiciones
                 </a>
               </p>
               <button
@@ -206,12 +206,12 @@ export default function Checkout() {
                 className="mt-4 inline-flex w-full items-center justify-center rounded bg-paypalYellow py-2.5 px-4 text-base font-semibold tracking-wide text-paypalBlue outline-none ring-offset-2 transition focus:bg-paypalYellow sm:text-lg"
               >
                 <GrPaypal className="mr-2 text-paypalBlue" />
-                Pay with PayPal
+                Pagar con PayPal
               </button>
             </div>
           </div>
           <div class="relative col-span-full flex flex-col py-6 pl-8 pr-4 sm:py-12 lg:col-span-4 lg:py-24">
-            <h2 class="sr-only">Order summary</h2>
+            <h2 class="sr-only">Resumen de compra</h2>
             <div>
               <img
                 src="https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
@@ -235,34 +235,34 @@ export default function Checkout() {
               <div class="my-5 h-0.5 w-full bg-white bg-opacity-30"></div>
               <div class="space-y-2">
                 <p class="flex justify-between text-sm font-medium text-white">
-                  <span>Vat: {vatPercentage}%</span>
-                  <span>${vatFormateado}</span>
+                  <span>IVA: {vatPercentage}%</span>
+                  <span>USD{vatFormateado}</span>
                 </p>
                 <p class="flex justify-between text-lg font-bold text-white">
-                  <span>Total price (incl. VAT):</span>
-                  <span>${precioFormateado}</span>
+                  <span>Precio total (incl. IVA):</span>
+                  <span>USD{precioFormateado}</span>
                 </p>
               </div>
             </div>
             <div class="relative mt-10 text-white">
-              <h3 class="mb-5 text-lg font-bold">Support</h3>
+              <h3 class="mb-5 text-lg font-bold">Atención al cliente</h3>
               <p class="text-sm font-semibold">
-                +01 653 235 211 <span class="font-light">(International)</span>
+                +01 653 235 211 <span class="font-light">(Internacional)</span>
               </p>
               <p class="mt-1 text-sm font-semibold">
                 support@wanderlust.com <span class="font-light">(Email)</span>
               </p>
               <p class="mt-2 text-xs font-medium">
-                Call us now for payment related issues
+                Contáctanos ahora por cuestiones relacionadas al pago.
               </p>
             </div>
             <div class="relative mt-10 flex">
               <p class="flex flex-col">
                 <span class="text-sm font-bold text-white">
-                  Money Back Guarantee
+                  Garantía de devolución del dinero
                 </span>
                 <span class="text-xs font-medium text-white">
-                  within 30 days of purchase
+                  dentro de los 30 días de la compra.
                 </span>
               </p>
             </div>
