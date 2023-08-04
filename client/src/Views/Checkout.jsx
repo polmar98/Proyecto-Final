e45936c01aa78c4a039c8fc2c65eed5107afc138
LucasAonzo <lucasaonzo@gmail.com>
2023-08-03 21:08:13 -0300

    Bug formregister solucionado

--- client/src/Views/Checkout.jsx
+++ client/src/Views/Checkout.jsx
@@ -59,11 +59,7 @@ export default function Checkout() {
       user_action: "PAY_NOW",
       return_url: "http://localhost:3002/payment/pay-order",
       cancel_url: "http://localhost:3002/payment/cancel-order",
-<<<<<<< HEAD
       current_user: currentUser.uid, //ACÁ HAY QUE TRAER EL UUID DEL CURRENT USER.
-=======
-      current_user: currentUser.uid,
->>>>>>> lucas
     },
   };
 
