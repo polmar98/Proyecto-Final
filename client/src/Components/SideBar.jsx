2f09e9bd9badb02b5f352e46f74b067e3247503d
LucasAonzo <lucasaonzo@gmail.com>
2023-08-05 13:34:06 -0300

    Agregando perfil user rating

--- client/src/Components/SideBar.jsx
+++ client/src/Components/SideBar.jsx
@@ -14,30 +14,26 @@ function Sidebar() {
     <div className="flex flex-col justify-between w-64 min-h-screen bg-white text-black px-6 py-4 rounded-lg shadow-lg overflow-auto">
       <ul>
         <li className="mb-6 flex items-center">
-          <BsHouseDoor className="mr-4" />
-          <Link to="/home">Home</Link>
-        </li>
-        <li className="mb-6 flex items-center">
           <BsBox className="mr-4" />
-          <Link to="/packages">Packages</Link>
+          <Link to="/userProfile/packages">Paquetes</Link>
         </li>
         <li className="mb-6 flex items-center">
           <BsGraphUp className="mr-4" />
-          <Link to="/activity">Activity</Link>
+          <Link to="/activity">Actividades</Link>
         </li>
         <li className="mb-6 flex items-center">
           <BsCardChecklist className="mr-4" />
-          <Link to="/itinerary">Itinerary</Link>
+          <Link to="/itinerary">Itinerario</Link>
         </li>
         <li className="mb-6 flex items-center">
           <BsPerson className="mr-4" />
-          <Link to="/profile">Profile</Link>
+          <Link to="/userProfile/profile">Perfil</Link>
         </li>
       </ul>
       <ul>
         <li className="mb-6 flex items-center">
           <BsBoxArrowInRight className="mr-4" />
-          <Link to="/logout">Logout</Link>
+          <Link to="/logout">Cerrar Sesion</Link>
         </li>
       </ul>
     </div>
--- client/src/Views/UserProfile.jsx
+++ client/src/Views/UserProfile.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom"; // Nota que importamos Routes y Route en lugar de useRoutes
 import { Link } from "react-router-dom";
 import { useAuth } from "../Context/authContext";
 import { useNavigate } from "react-router-dom";
@@ -6,10 +7,10 @@ import { toast } from "react-toastify";
 import NavBar from "../Components/NavBar";
 import Sidebar from "../Components/SideBar";
 import UserDate from "../Components/Userprofile/UserDetail";
+import Packages from "../Components/Userprofile/Packages";
 
 const UserProfile = () => {
   const { currentUser, logout } = useAuth();
-  const navigate = useNavigate();
 
   const handleLogoutClick = (event) => {
     event.preventDefault();
@@ -25,7 +26,12 @@ const UserProfile = () => {
 
       <div className="flex max-h-screen overflow-auto">
         <Sidebar />
-        <UserDate />
+        <Routes>
+          <Route path="/" element={<Navigate to="packages" />} />{" "}
+          {/* Redirección */}
+          <Route path="profile" element={<UserDate />} />
+          <Route path="packages" element={<Packages />} />
+        </Routes>
       </div>
     </div>
   );
