98e5c874f28ce7ed7c0ef9d6d75805546dd8e54b
German <german2717@gmail.com>
2023-08-05 21:06:41 -0300

    admin-user funcionando

--- client/src/Views/Admin.jsx
+++ client/src/Views/Admin.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import NavBar from "../Components/NavBar";
 import SideBarAdmin from "../Components/SideBarAdmin";
-
+import { MdOutlineAdminPanelSettings } from "react-icons/md";
 function Admin() {
   return (
     <>
-      <div className="relative bg-verdeFooter">
+      <div className="relative bg-red-600 ">
         <NavBar />
+        <div className="flex justify-start">
+          <span className=" text-xl fonte flex text-gray-100 -my-1 fontPoppins ml-[10%]">
+            <MdOutlineAdminPanelSettings className="mr-1" />
+            Administrador
+          </span>
+        </div>
       </div>
       <div className="flex max-h-screen overflow-auto">
         <SideBarAdmin />
