fb55937c5cec0912f9990ec3c470e53563bfeced
LucasAonzo <lucasaonzo@gmail.com>
2023-08-06 15:08:43 -0300

    modificando detail esteticamente

--- client/src/Components/Activities.jsx
+++ client/src/Components/Activities.jsx
@@ -3,7 +3,7 @@ import { authContext } from "../Context/authContext";
 import { useDispatch, useSelector } from "react-redux";
 // import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
-import {add_to_cart} from '../Redux/ShoppingCart/shoppingCartActions';
+import { add_to_cart } from "../Redux/ShoppingCart/shoppingCartActions";
 import { userShopping } from "../Redux/ShoppingCart/shoppingCartActions";
 
 function Activities({ activity }) {
@@ -13,35 +13,34 @@ function Activities({ activity }) {
   // const car = useSelector((state) => state.carrito.cart);
   const { currentUser } = useContext(authContext);
   // console.log('el carrito logueada', car)
-  
-//  console.log("actividades en Act: ", Activities); // aca llegan las 4 actividades
+
+  //  console.log("actividades en Act: ", Activities); // aca llegan las 4 actividades
 
   const handleReserveActivity = (selectedActivity) => {
     //addNew(selectedActivity);
     const item = selectedActivity;
-    console.log('actividad reservada', selectedActivity)
+    console.log("actividad reservada", selectedActivity);
 
     if (currentUser) {
       dispatch(add_to_cart(item));
       guardarEnBDD(item);
       dispatch(userShopping(currentUser.uid));
     } else {
-        addNewItem(item);
+      addNewItem(item);
     }
   };
 
-
-  const guardarEnBDD = async(item) => {
-     const response1 = await fetch(
-          `http://localhost:3002/shoppingCar/${idCart}`,
-          {
-            method: "PUT",
-            headers: { "Content-Type": "application/json",},
-            body: JSON.stringify(item),
-          }
-     );
+  const guardarEnBDD = async (item) => {
+    const response1 = await fetch(
+      `http://localhost:3002/shoppingCar/${idCart}`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(item),
+      }
+    );
   };
-  
+
   //agregar items al localStorage
   function addNewItem(item) {
     let localStorageJSON = localStorage.getItem("carrito");
@@ -51,78 +50,64 @@ function Activities({ activity }) {
     }
     storedItems.push(item);
     const updatedItemsJSON = JSON.stringify(storedItems);
-     localStorage.setItem("carrito", updatedItemsJSON); //lo convierte a json
-  };
-
+    localStorage.setItem("carrito", updatedItemsJSON); //lo convierte a json
+  }
 
   return (
     <>
       <h2 className="text-xl font-bold mb-4 mt-8 text-left fontPoppins">
-        Actividades 
+        Actividades
       </h2>
       <div className="grid grid-cols-2 gap-4 mt-8">
         {Activities?.map((el) => (
           <div
             key={el.id}
-            className="block rounded-lg bg-white shadow-xl border border-gray-300 p-4 card hover:scale-110 hover:z-20"
+            className="block rounded-lg bg-white shadow-xl border border-gray-300 p-4 card transform hover:scale-105 hover:z-10 transition-all duration-200"
           >
             <a href="#!">
               <img
-                className="rounded-t-lg w-full h-auto"
+                className="rounded-t-lg w-full h-60 object-cover"
                 src={
                   el.image
                     ? el.image
                     : "https://uss.com.ar/sitio/wp-content/themes/consultix/images/no-image-found-360x260.png"
                 }
-                alt="Img not found"
+                alt="Activity"
               />
             </a>
-            <div className="text-center fontPoppins">
-              <h5 className="mb-4 mt-2 text-m font-bold leading-tight text-gray-800">
+            <div className="mt-4 text-center fontPoppins">
+              <h5 className="text-lg font-bold leading-tight text-gray-800">
                 {el.name}
               </h5>
-
-              <div>
-                {!el.included ? (
-                  <div>
-                    <span className="text-lg fontPoppinsB  mr-2 text-red-600 mb-4">
-                      Reservalo YA pagando solo USD {el.price}!!!
-                    </span>
-                    <div className="flex justify-between">
-                      <p className=" text-sm ml-2 text-gray-600">
-                        Duración: {el.duration}
-                      </p>
-                    </div>
-                    <div>
-
-                        <button
-                          onClick={() => {
-                              handleReserveActivity(
-                                {
-                                  amount: 1,
-                                  unitPrice: Number(el.price),
-                                  totalPrice: Number(el.price),
-                                  typeProduct: 2,
-                                  idProduct:el.id,
-                                  title: el.name,
-                                  image: el.image,
-                                }
-                              )
-                              toast.success("Actividad reservada");
-                              // window.alert("Actividad reservada");
-                          
-                          }}
-                          className="p-1 text-yellow-500 fontPoppinsB scale-150 rounded bg-red-600 hover:rotate-12 transition "
-                        >
-                          Reservar
-                        </button>
-                     
-                    </div>
-                  </div>
-                ) : (
-                  <span>Actividad incluida</span>
-                )}
-              </div>
+              <p className="text-gray-500">
+                {!el.included
+                  ? `Duración: ${el.duration}`
+                  : "Actividad incluida"}
+              </p>
+              {!el.included && (
+                <div className="mt-4 flex flex-col">
+                  <span className="text-lg fontPoppinsB text-red-600 mb-4 inline-block">
+                    Reservalo YA pagando solo USD {el.price}!!!
+                  </span>
+                  <button
+                    onClick={() => {
+                      handleReserveActivity({
+                        amount: 1,
+                        unitPrice: Number(el.price),
+                        totalPrice: Number(el.price),
+                        typeProduct: 2,
+                        idProduct: el.id,
+                        title: el.name,
+                        image: el.image,
+                      });
+                      toast.success("Actividad reservada");
+                    }}
+                    className="p-2 text-white fontPoppinsB text-sm rounded bg-red-600 hover:bg-red-700 transition-colors mt-2"
+                  >
+                    Reservar
+                  </button>
+                </div>
+              )}
             </div>
           </div>
         ))}
--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./Context/authContext";
 import "flowbite";
 import "@tremor/react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
