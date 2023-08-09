46f06713c2458bf9c516460c2a2e6b001b75718b
LucasAonzo <lucasaonzo@gmail.com>
2023-08-08 21:52:37 -0300

    modificando pantalla nosotros

--- client/src/Views/About.jsx
+++ client/src/Views/About.jsx
@@ -1,58 +1,156 @@
 import React from "react";
 import NavBar from "../Components/NavBar";
+import {
+  FaPlane,
+  FaHotel,
+  FaMapMarkedAlt,
+  FaSuitcaseRolling,
+  FaCar,
+  FaUmbrellaBeach,
+} from "react-icons/fa";
+import Statistic from "../Components/Statistic";
+import Content from "../Components/Content";
+import Footer from "../Components/Footer";
 
 function About() {
   return (
-    <div className="bg-white min-h-screen">
+    <>
       <div className="bg-verdeFooter shadow-md text-white">
         <NavBar />
       </div>
-      <div className="text-center p-10 max-w-5xl mx-auto">
-        <h2 className="text-4xl font-bold mb-6 text-gray-800">
-          Bienvenidos a Wanderlust
-        </h2>
-        <p className="mt-6 px-8 text-gray-700 text-xl leading-relaxed">
-          Donde encontrarás experiencias de viaje inolvidables y descubrirás los
-          destinos más fascinantes del mundo. Nuestro proyecto nace de la
-          colaboración de un grupo de entusiastas viajeros que se conocieron
-          durante un bootcamp de desarrollo web. Compartimos la visión de crear
-          una plataforma que no solo ofrezca servicios de viaje, sino que
-          también inspire a las personas a explorar nuevos horizontes y
-          sumergirse en diferentes culturas. Creemos en la importancia de los
-          detalles y la personalización. Por eso, nos esforzamos en diseñar
-          paquetes turísticos que se adapten a tus preferencias y necesidades
-          individuales. Ya sea que estés buscando unas vacaciones relajantes en
-          una playa paradisíaca, un emocionante viaje de aventura o una escapada
-          cultural, estamos aquí para convertir tus sueños en realidad. Te
-          invitamos a explorar nuestro sitio web y descubrir las maravillas que
-          te esperan. Estamos emocionados de ser parte de tu próxima aventura y
-          ayudarte a crear momentos que perdurarán toda la vida. ¡Viaja con
-          nosotros y deja que tu espíritu aventurero se desate!
-        </p>
-        <h3 className="text-3xl font-semibold mt-12 text-gray-800">
-          Desarrolladores
-        </h3>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
-          <Card name="Lucas David Aonzo" role="Frontend" />
-          <Card name="Moira Magali Brun" role="Frontend" />
-          <Card name="German Efrain Montenegro Rolon" role="Frontend" />
-          <Card name="Sara Carolina Marulanda Rojas" role="Frontend" />
-          <Card name="Paul Fernando Martinez Navarro" role="Backend" />
-          <Card name="Cristian David Molina" role="Backend" />
-          <Card name="Eliana Soledad Santa Cruz Torres" role="Backend" />
-          <Card name="Ivan Jose Trejo Contreras" role="Backend" />
+      <div>
+        <img
+          src="https://wanderlust.com.ua/img/shop_background_new.jpg"
+          alt=""
+        />
+      </div>
+      <Statistic />
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 animate-fade-up">
+        <div className="p-8 rounded shadow-xl sm:p-16">
+          <div className="flex flex-col lg:flex-row">
+            <div className="mb-6 lg:mb-0 lg:w-1/2 lg:pr-5">
+              <h2 className="font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                Tu aventura comienza aquí
+                <br className="hidden md:block " />
+                <span className="inline-block text-textGH ">
+                  Descubre, viaja y vive
+                </span>{" "}
+                experiencias únicas
+              </h2>
+            </div>
+            <div className="lg:w-1/2 text-left">
+              <p className="mb-4 text-base text-gray-700">
+                En Wanderlust, nos apasiona viajar. Sabemos que cada viaje es
+                una historia que contar, por eso nos dedicamos a garantizar que
+                cada aventura sea única e inolvidable. Desde vuelos económicos,
+                hoteles acogedores hasta actividades emocionantes, cuidamos cada
+                detalle para que tú solo te preocupes de disfrutar. Con años de
+                experiencia y un equipo dedicado, nos esforzamos por hacer que
+                tus sueños viajeros se conviertan en realidad.
+              </p>
+              <a
+                href="/"
+                aria-label=""
+                className="text-textGH inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+              >
+                Learn more
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+      <section className="bg-white py-12 sm:py-16 lg:py-20">
+        <div className="mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-lg">
+          <div className="text-center">
+            <h2 className="text-3xl font-bold text-slate-800 sm:text-4xl xl:text-5xl">
+              Descubre nuevas aventuras
+            </h2>
+            <p className="mt-4 text-base text-gray-700 sm:mt-8">
+              Sumérgete en experiencias únicas y lugares inolvidables. Deja que
+              tu espíritu aventurero nos elija. Con nuestros paquetes de viaje,
+              la aventura está garantizada.
+            </p>
+          </div>
 
-function Card({ name, role }) {
-  return (
-    <div className="p-6 shadow-sm rounded-md bg-white transition duration-300 transform hover:shadow-md">
-      <h4 className="font-semibold text-2xl mb-2 text-gray-800">{name}</h4>
-      <p className="font-light text-lg text-gray-600">{role}</p>
-    </div>
+          <div className="sm:col-gap-12 row-gap-12 md:gap mt-10 grid grid-cols-1 sm:mt-16 sm:grid-cols-2 md:grid-cols-3 xl:mt-24">
+            <div className="md:border-b-2 border-emerald-500 md:shadow-lg m-2 md:p-8 lg:p-12 animate-fade">
+              <FaPlane
+                className="mx-auto block align-middle text-textGH"
+                size={46}
+              />
+              <h3 className="mt-12 text-xl font-bold text-slate-800">Vuelos</h3>
+              <p className="mt-5 text-base text-gray-700">
+                Conexiones aéreas a los destinos más buscados.
+              </p>
+            </div>
+
+            <div className="md:border-b-2 border-emerald-500 md:shadow-lg m-2 md:p-8 lg:p-12 animate-fade">
+              <FaHotel
+                className="mx-auto block align-middle text-textGH"
+                size={46}
+              />
+              <h3 className="mt-12 text-xl font-bold text-slate-800">
+                Hospedajes
+              </h3>
+              <p className="mt-5 text-base text-gray-700">
+                Las mejores opciones para descansar en tu viaje.
+              </p>
+            </div>
+
+            <div className="md:border-b-2  border-emerald-500 md:shadow-lg m-2 md:p-8 lg:p-12 animate-fade">
+              <FaMapMarkedAlt
+                className="mx-auto block align-middle text-textGH"
+                size={46}
+              />
+              <h3 className="mt-12 text-xl font-bold text-slate-800">
+                Actividades y Tours
+              </h3>
+              <p className="mt-5 text-base text-gray-700">
+                Descubre y reserva las mejores actividades.
+              </p>
+            </div>
+
+            <div className="md:border-b-2 border-emerald-500 md:shadow-lg m-2 md:p-8 lg:p-12 animate-fade">
+              <FaSuitcaseRolling
+                className="mx-auto block align-middle text-textGH"
+                size={46}
+              />
+              <h3 className="mt-12 text-xl font-bold text-slate-800">
+                Paquetes
+              </h3>
+              <p className="mt-5 text-base text-gray-700">
+                Combina y ahorra con nuestros paquetes de viaje.
+              </p>
+            </div>
+
+            <div className="md:border-b-2 border-emerald-500 md:shadow-lg m-2 md:p-8 lg:p-12 animate-fade ">
+              <FaCar
+                className="mx-auto block align-middle text-textGH"
+                size={46}
+              />
+              <h3 className="mt-12 text-xl font-bold text-slate-800">
+                Alquiler de autos
+              </h3>
+              <p className="mt-5 text-base text-gray-700">
+                Explora a tu ritmo con nuestras opciones de renta.
+              </p>
+            </div>
+
+            <div className="md:border-b-2 border-emerald-500 md:shadow-lg m-2 md:p-8 lg:p-12 animate-fade">
+              <FaUmbrellaBeach
+                className="mx-auto block align-middle text-textGH"
+                size={46}
+              />
+              <h3 className="mt-12 text-xl font-bold text-slate-800">Playas</h3>
+              <p className="mt-5 text-base text-gray-700">
+                Relájate en las playas más hermosas del mundo.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
   );
 }
 
